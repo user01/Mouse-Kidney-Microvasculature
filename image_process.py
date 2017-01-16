5ef92e844a88e3d14b5a6b832316d8e7916bdd62
Erik <erik.langenborg@gmail.com>
2017-01-16 00:32:00 -0500

    Basic mesh creation - wrong thresholds and far too large

--- image_process.py
+++ image_process.py
@@ -5,6 +5,7 @@ import os
 import numpy as np
 from skimage import measure
 import matplotlib.image as mpimg
+from stl import mesh
 
 
 def downsample_array(nn, side_start, chunk_size):
@@ -32,3 +33,14 @@ faces = sorted(os.listdir(os.path.join(
     'ii-3001-1-mouse-kidney_1GV', 'source')))
 
 images = np.array(list(map(image_load, faces)))
+
+verts, faces = measure.marching_cubes(images, 0.5, spacing=(0.7*8, 0.7*8, 6.0))
+
+
+
+new_mesh = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
+for i, f in enumerate(faces):
+    for j in range(3):
+        new_mesh.vectors[i][j] = verts[f[j],:]
+
+new_mesh.save('simple.stl')
